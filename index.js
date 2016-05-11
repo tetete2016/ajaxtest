2b002deab10b6e2bac9261e9118a19b15af816cc
tetete2016 <tetetelego2013@gmail.com>
2016-05-11 18:08:56 +0900

    fixed error

--- index.js
+++ index.js
@@ -7,12 +7,13 @@ app.set('port', (process.env.PORT || 5000));
 app.use(express.static('public'));
 
 app.get('/', function (request, response) {
-    response.send(x);
+    console.log(request);
+    response.send("x=" + x);
     x++;
 });
 
-app.listen(app.get('port'), function() {
-  console.log('Node app is running on port', app.get('port'));
+app.listen(app.get('port'), function () {
+    console.log('Node app is running on port', app.get('port'));
 });
 
 
