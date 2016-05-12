aa5248eacca97f14e069bf33b64874e9e70c7cdc
tetete2016 <tetetelego2013@gmail.com>
2016-05-12 21:51:01 +0900

    added highscore test

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 var express = require('express');
 var app = express();
-var x = 0;
 
 app.set('port', (process.env.PORT || 5000));
 
@@ -9,6 +8,14 @@ app.use(express.static('public'));
 app.get('/serverside', function (request, response) {
     console.log(request.query);
     response.send(JSON.stringify(request.query));
+});
+
+app.post('/highscore', function (request, response) {
+    console.log(JSON.stringify(request.query));
+});
+
+app.get('/highscore', function (request, response) {
+    console.log("high score get");
 });
 
 app.listen(app.get('port'), function () {
